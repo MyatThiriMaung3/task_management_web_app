ff400a240a00cd31238af9e9b47730afaa244e4b
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-03-09 03:14:20 +0700

    Design Profile Screen

--- server.js
+++ server.js
@@ -10,4 +10,16 @@ app.get("/", (req, res) => {
     res.render("dashboard")
 })
 
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+app.get("/profile", (req, res) => {
+    res.render("profile")
+})
+
 app.listen(3000)

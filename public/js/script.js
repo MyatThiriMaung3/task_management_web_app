b7b89fd156d0c05efc94728e10538a42018eb7c8
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-03-09 01:22:50 +0700

    Design Dashboard Screen

--- public/js/script.js
+++ public/js/script.js
@@ -20,3 +20,19 @@ document.getElementById("signupForm").addEventListener("submit", function(event)
         alert("Passwords do not match. Please try again.");
     }
 });
+
+function toggleMenu(menuId) {
+    const menu = document.getElementById(menuId);
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+// Close menu when clicking outside
+document.addEventListener("click", function(event) {
+    const menu = document.getElementById("menuDropdown");
+    const menuIcon = document.querySelector(".ic-menu-size");
+
+    if (!menu.contains(event.target) && event.target !== menuIcon) {
+        menu.style.display = "none";
+    }
+});
+
